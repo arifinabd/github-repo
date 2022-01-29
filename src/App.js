769113171ed121acb20c6414d269e383bf28b654
abdurrahman arifin <abdurrahman.arifin189@gmail.com>
2022-01-29 14:23:33 +0700

    get API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import { useState } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
   const [username, setUsername] = useState("");
   const [loading, setLoading] = useState(false);
+  const [repos, setRepos] = useState([]);
 
-  function handleSubmit() {}
+  function handleSubmit(e) {
+    e.preventDefault();
+    searchRepos();
+  }
+
+  function searchRepos() {
+    setLoading(true);
+    axios({
+      method: "get",
+      url: `https://api.github.com/users/${username}/repos`,
+    }).then((res) => {
+      setLoading(false);
+      setRepos(res.data);
+    });
+    console.log(repos);
+  }
 
   return (
     <div className="page">
