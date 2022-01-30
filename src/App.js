7fb51d9fc5b5543a7cf039057ec2040e2cdfd8ac
abdurrahman arifin <abdurrahman.arifin189@gmail.com>
2022-01-30 09:45:02 +0700

    new commit

--- src/App.js
+++ src/App.js
@@ -51,11 +51,12 @@ function App() {
   return (
     <div className="page">
       <div className="landing-page-container">
+        <p>by Abdurrahman Arifin</p>
         <div className="left-side">
           <form className="form">
             <input className="input" value={username} placeholder="Github Username" onChange={(e) => setUsername(e.target.value)} />
             <button className="button" onClick={handleSubmit}>
-              {loading ? "Searching... " : "Search"}
+              {loading ? "searching... " : "search"}
             </button>
           </form>
           <div className="results-container">{repos.map(renderRepo)}</div>
